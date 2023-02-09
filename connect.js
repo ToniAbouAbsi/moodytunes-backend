2cfec213e5b6d334b445004624e91bd55c6a93df
ToniAbouAbsi <81428375+ToniAbouAbsi@users.noreply.github.com>
2023-02-09 18:27:13 +0200

    Adding Online DB Variables

--- connect.js
+++ connect.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'TAA#1011',
-  database: 'moody',
+  host: 'sql12.freesqldatabase.com',
+  user: 'sql12597061',
+  password: 'aiAi4iNxce',
+  database: 'sql12597061',
 });
