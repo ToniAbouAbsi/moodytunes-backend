8c3036982e52a585673d39ef5dd3f1ccc8b1fb10
ToniAbouAbsi <81428375+ToniAbouAbsi@users.noreply.github.com>
2023-02-09 19:10:57 +0200

    Adding Online DB Variables2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import authRoutes from './routes/auth.js';
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://moodytunes.onrender.com',
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
